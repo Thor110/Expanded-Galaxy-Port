9f18c3e0f41cd2876650b3794dd3b748c77a7452
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-07-31 07:05:51 +0100

    Installer Updated
    Now requests user input if multiple versions of the game are detected.

--- Expanded Galaxy Port Installer/use-this-install-please.bat
+++ Expanded Galaxy Port Installer/use-this-install-please.bat
@@ -1,6 +1,8 @@
 title Expanded Galaxy Project Installer
 
 @echo off
+set tempA = 0
+set tempB = 0
 
 Rem KOTOR1 DISC
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\BioWare\SW\KOTOR /v Path /s
@@ -15,7 +17,7 @@ Rem KOTOR1 GOG
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\GOG.com\Games\1207666283 /v Path /s
 if %errorlevel% equ 0 (
   Rem echo "kotor 1 gog registry entry detected!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\GOG.com\Games\1207666283" /v "Path"') do set mykey1=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\GOG.com\Games\1207666283" /v "Path"') do set mykey2=%%b
 ) else (
   Rem echo "kotor 1 gog registry entry not detected!"
 )
@@ -24,7 +26,7 @@ Rem KOTOR1 STEAM
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 32370" /v InstallLocation /s
 if %errorlevel% equ 0 (
   Rem echo "kotor 1 steam registry entry detected!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 32370" /v "InstallLocation"') do set mykey1=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 32370" /v "InstallLocation"') do set mykey3=%%b
 ) else (
   Rem echo "kotor 1 steam registry entry not detected!"
 )
@@ -33,7 +35,7 @@ Rem KOTOR2 DISC
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\LucasArts\KotOR2 /v Path /s
 if %errorlevel% equ 0 (
   echo "Hello World! Loyal fan & owner of a disc copy of the game!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\WOW6432Node\LucasArts\KotOR2" /v "Path"') do set mykey2=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\WOW6432Node\LucasArts\KotOR2" /v "Path"') do set mykey4=%%b
 ) else (
   Rem echo "kotor 2 disc registry entry not detected!"
 )
@@ -42,7 +44,7 @@ Rem KOTOR2 GOG
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\GOG.com\Games\1421404581 /v PATH /s
 if %errorlevel% equ 0 (
   echo "Hello World! Good old Games User!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SSOFTWARE\GOG.com\Games\1421404581" /v "PATH"') do set mykey2=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SSOFTWARE\GOG.com\Games\1421404581" /v "PATH"') do set mykey5=%%b
 ) else (
   Rem echo "kotor 2 gog registry entry not detected!"
 )
@@ -51,7 +53,7 @@ Rem KOTOR2 STEAM
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 208580" /v InstallLocation /s
 if %errorlevel% equ 0 (
   echo "Hello World! Steam User!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 208580" /v "InstallLocation"') do set mykey2=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 208580" /v "InstallLocation"') do set mykey6=%%b
 ) else (
   Rem echo "kotor 2 steam registry entry not detected!"
 )
@@ -59,45 +61,120 @@ if %errorlevel% equ 0 (
 cls
 
 if defined mykey1 (
-echo "Star Wars : Knights of the Old Republic Installation Detected!"
-) else (
-echo "Star Wars : Knights of the Old Republic Installation Not Detected!"
-pause
-exit
+  set /a tempA=%tempA%+1
 )
 
 if defined mykey2 (
-echo "Star Wars : Knights of the Old Republic II Installation Detected!"
-) else (
-echo "Star Wars : Knights of the Old Republic II Installation Not Detected!"
-pause
-exit
+  set /a tempA=%tempA%+1
 )
 
-if exist "%mykey1%\chitin.key" (
-  Rem echo Game Found.
+if defined mykey3 (
+  set /a tempA=%tempA%+1
+)
+
+if tempA == 0 goto INPUT2
+
+:INPUT1
+echo Please enter which install of the first game to use.
+echo Disc Version = 1
+echo GoG Version = 2
+echo Steam Version = 3
+set /p InstallA="Which install to use: "
+echo %InstallA%|findstr /r "[^0-9]" && (
+    goto :INPUT1
+)
+if %InstallA% gtr 3  (
+   echo enter a number between 1 and 3
+   goto :INPUT1
+)
+if %InstallA% lss 1 (
+   echo enter a number between 1 and 3
+   goto :INPUT1
+)
+
+if %InstallA% == 1 set mykeyA=%mykey1%
+if %InstallA% == 2 set mykeyA=%mykey2%
+if %InstallA% == 3 set mykeyA=%mykey3%
+
+Rem one temp variable next time
+
+if defined mykey4 (
+  set /a tempB=%tempB%+1
+)
+
+if defined mykey5 (
+  set /a tempB=%tempB%+1
+)
+
+if defined mykey6 (
+  set /a tempB=%tempB%+1
+)
+
+Rem one temp variable next time
+
+:INPUT2
+
+if tempB == 0 goto INSTALL
+
+echo Please enter which install of the second game to use.
+echo Disc Version = 1
+echo GoG Version = 2
+echo Steam Version = 3
+set /p InstallB="Which install to use: "
+echo %InstallB%|findstr /r "[^0-9]" && (
+   goto :INPUT2
+)
+if %InstallB% gtr 3  (
+   echo enter a number between 1 and 3
+   goto :INPUT2
+)
+if %InstallB% lss 1 (
+   echo enter a number between 1 and 3
+   goto :INPUT2
+)
+
+if %InstallB% == 1 set mykeyB=%mykey4%
+if %InstallB% == 2 set mykeyB=%mykey5%
+if %InstallB% == 3 set mykeyB=%mykey6%
+
+:INSTALL
+
+pause
+
+echo testing
+
+pause
+
+echo testing
+
+pause
+
+if exist "%mykeyA%\chitin.key" (
+  echo "Star Wars : Knights of the Old Republic Installation Detected!"
 ) else (
-  Rem echo Game Not Found!
+  echo "Star Wars : Knights of the Old Republic Installation Not Detected!"
+  pause
   exit
 )
 
-if exist "%mykey2%\chitin.key" (
-  Rem echo Game Found.
+if exist "%mykeyB%\chitin.key" (
+  echo "Star Wars : Knights of the Old Republic II Installation Detected!"
 ) else (
-  Rem echo Game Not Found!
+  echo "Star Wars : Knights of the Old Republic II Installation Not Detected!"
+  pause
   exit
 )
 
 echo "Installing Expanded Galaxy Project Port"
 
-FOR /F "delims=" %%a IN (tslpatchdata\lips-file-list.txt) DO COPY "%mykey1%\lips\%%~a" "%mykey2%\lips\%%~nxa"
-FOR /F "delims=" %%a IN (tslpatchdata\movies-file-list.txt) DO COPY "%mykey1%\movies\%%~a" "%mykey2%\movies\%%~nxa"
-FOR /F "delims=" %%a IN (tslpatchdata\streammusic-file-list.txt) DO COPY "%mykey1%\streammusic\%%~a" "%mykey2%\streammusic\%%~nxa"
-FOR /F "delims=" %%a IN (tslpatchdata\streamsounds-file-list.txt) DO COPY "%mykey1%\streamsounds\%%~a" "%mykey2%\streamsounds\%%~nxa"
-FOR /F "delims=" %%a IN (tslpatchdata\streamwaves-file-list.txt) DO COPY "%mykey1%\streamwaves%%~a" "%mykey2%\streamvoice%%~nxa"
-copy /y "%mykey1%\movies\biologo.bik" "%mykey2%\movies\ObsidianEnt.bik"
+FOR /F "delims=" %%a IN (tslpatchdata\lips-file-list.txt) DO COPY "%mykeyA%\lips\%%~a" "%mykeyB%\lips\%%~nxa"
+FOR /F "delims=" %%a IN (tslpatchdata\movies-file-list.txt) DO COPY "%mykeyA%\movies\%%~a" "%mykeyB%\movies\%%~nxa"
+FOR /F "delims=" %%a IN (tslpatchdata\streammusic-file-list.txt) DO COPY "%mykeyA%\streammusic\%%~a" "%mykeyB%\streammusic\%%~nxa"
+FOR /F "delims=" %%a IN (tslpatchdata\streamsounds-file-list.txt) DO COPY "%mykeyA%\streamsounds\%%~a" "%mykeyB%\streamsounds\%%~nxa"
+FOR /F "delims=" %%a IN (tslpatchdata\streamwaves-file-list.txt) DO COPY "%mykeyA%\streamwaves%%~a" "%mykeyB%\streamvoice%%~nxa"
+copy /y "%mykeyA%\movies\biologo.bik" "%mykeyB%\movies\ObsidianEnt.bik"
 
-tslpatchdata\pykotorcli.exe "%mykey2%" "%cd%"
+tslpatchdata\pykotorcli.exe "%mykeyB%" "%cd%"
 
 cls
 
