f983f29717692e7fe1ccc0c8d2975e51b2fedd7c
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-08-10 14:27:59 +0100

    Launcher Updated

--- a-template/launcher.bat
+++ a-template/launcher.bat
@@ -34,44 +34,59 @@ if %Choice% == 2 goto :KOTOR2
 
 :KOTOR1
 
+if exist dialog.tlk.main goto :STEAMORDISC
+
+ren Movies\ObsidianEnt.bik ObsidianEnt.bik.main
+
+ren dialog.tlk dialog.tlk.main
+ren lips\001EBO_loc.mod 001EBO_loc.mod.main
+ren Modules\001ebo.mod 001ebo.mod.main
+ren StreamMusic\mus_a_503.wav mus_a_503.wav.main
+ren StreamMusic\mus_sion.wav mus_sion.wav.main
+
+for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.main"
+
+ren Movies\ObsidianEnt.bik.port ObsidianEnt.bik
+
+ren dialog.tlk.port dialog.tlk
+ren lips\001EBO_loc.mod.port 001EBO_loc.mod
+ren Modules\001ebo.mod.port 001ebo.mod
+ren StreamMusic\mus_a_503.wav.port mus_a_503.wav
+ren StreamMusic\mus_sion.wav.port mus_sion.wav
+
+for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.port" "%%i"
+
 echo Launching KotOR1 in KotOR2!
 
-if exist dialog.tlk.port goto :INPUT4
-
-goto :INPUT1
+goto :STEAMORDISC
 
 :KOTOR2
 
+if exist dialog.tlk.port goto :STEAMORDISC
+
+ren Movies\ObsidianEnt.bik ObsidianEnt.bik.port
+
+ren dialog.tlk dialog.tlk.port
+ren lips\001EBO_loc.mod 001EBO_loc.mod.port
+ren Modules\001ebo.mod 001ebo.mod.port
+ren StreamMusic\mus_a_503.wav mus_a_503.wav.port
+ren StreamMusic\mus_sion.wav mus_sion.wav.port
+
+for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.port"
+
+ren Movies\ObsidianEnt.bik.main ObsidianEnt.bik
+
+ren dialog.tlk.main dialog.tlk
+ren lips\001EBO_loc.mod.main 001EBO_loc.mod
+ren Modules\001ebo.mod.main 001ebo.mod
+ren StreamMusic\mus_a_503.wav.main mus_a_503.wav
+ren StreamMusic\mus_sion.wav.main mus_sion.wav
+
+for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.main" "%%i"
+
 echo Launching KotOR2!
 
-if exist dialog.tlk.main goto :INPUT3
-
-goto :INPUT2
-
-:INPUT1
-cls
-echo KotOR1 is currently enabled. Play Game!
-pause
 goto :STEAMORDISC
-exit
-
-:INPUT2
-cls
-echo KotOR2 is currently enabled. Play Game!
-pause
-goto :STEAMORDISC
-exit
-
-:INPUT3
-cls
-echo KotOR1 is currently enabled! Please enable KotOR2!
-pause
-port.bat
-:INPUT4
-cls
-echo KotOR2 is currently enabled! Please enable KotOR1!
-pause
-port.bat
 
 :STEAMORDISC
 if exist steam_api.dll goto :STEAM
