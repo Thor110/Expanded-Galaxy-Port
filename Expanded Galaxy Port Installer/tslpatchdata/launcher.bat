76c0d64eadab433ae4f660b5751692bd7f633595
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-08-12 11:14:45 +0100

    Launcher Updated
    File removed from rename list.

--- Expanded Galaxy Port Installer/tslpatchdata/launcher.bat
+++ Expanded Galaxy Port Installer/tslpatchdata/launcher.bat
@@ -41,7 +41,6 @@ ren Movies\ObsidianEnt.bik ObsidianEnt.bik.main
 ren dialog.tlk dialog.tlk.main
 ren lips\001EBO_loc.mod 001EBO_loc.mod.main
 ren Modules\001ebo.mod 001ebo.mod.main
-ren StreamMusic\mus_a_503.wav mus_a_503.wav.main
 ren StreamMusic\mus_sion.wav mus_sion.wav.main
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.main"
@@ -51,7 +50,6 @@ ren Movies\ObsidianEnt.bik.port ObsidianEnt.bik
 ren dialog.tlk.port dialog.tlk
 ren lips\001EBO_loc.mod.port 001EBO_loc.mod
 ren Modules\001ebo.mod.port 001ebo.mod
-ren StreamMusic\mus_a_503.wav.port mus_a_503.wav
 ren StreamMusic\mus_sion.wav.port mus_sion.wav
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.port" "%%i"
@@ -69,7 +67,6 @@ ren Movies\ObsidianEnt.bik ObsidianEnt.bik.port
 ren dialog.tlk dialog.tlk.port
 ren lips\001EBO_loc.mod 001EBO_loc.mod.port
 ren Modules\001ebo.mod 001ebo.mod.port
-ren StreamMusic\mus_a_503.wav mus_a_503.wav.port
 ren StreamMusic\mus_sion.wav mus_sion.wav.port
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.port"
@@ -79,7 +76,6 @@ ren Movies\ObsidianEnt.bik.main ObsidianEnt.bik
 ren dialog.tlk.main dialog.tlk
 ren lips\001EBO_loc.mod.main 001EBO_loc.mod
 ren Modules\001ebo.mod.main 001ebo.mod
-ren StreamMusic\mus_a_503.wav.main mus_a_503.wav
 ren StreamMusic\mus_sion.wav.main mus_sion.wav
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.main" "%%i"
