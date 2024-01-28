1e2aa317e3987f85556e4098309a747289755080
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-01-28 22:25:44 +0000

    release script fix

--- create-release.bat
+++ create-release.bat
@@ -2,7 +2,7 @@
 
 for /f "delims=" %%x in (version.txt) do set Build=port-v%%x
 
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.7z" "%cd%\Expanded Galaxy Port Installer\*"
+"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\Expanded Galaxy Port Installer\*"
 
 set fld=Override
 call :ADD
@@ -10,7 +10,7 @@ call :ADD
 md tslpatchdata
 for /r "Modules" %%x in (*.mod) do copy "%%x" "tslpatchdata\"
 copy dialog.tlk tslpatchdata\dialog.tlk
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.7z" "%cd%\tslpatchdata"
+"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\tslpatchdata"
 rd /s /q tslpatchdata
 
 echo release zipped
@@ -19,6 +19,6 @@ exit
 
 :ADD
 ren %fld% tslpatchdata
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.7z" "%cd%\tslpatchdata"
+"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\tslpatchdata"
 ren tslpatchdata %fld%
 exit /b
