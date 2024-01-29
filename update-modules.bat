d35211979746edbe13b5098b0d705050470dea2b
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-01-29 01:35:00 +0000

    Modules Updated

--- update-modules.bat
+++ update-modules.bat
@@ -1,11 +1,7 @@
 @echo off
 
-for /f "delims=" %%x in (version.txt) do set Build=port-v%%x
-
 for /r "Modules Source" %%x in (*.mod) do copy "%%x" "%cd%\Expanded Galaxy Port Installer\tslpatchdata\Modules"
 
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\Expanded Galaxy Port Installer\*"
-
-echo release zipped
+echo Modules Updated
 pause
 exit
