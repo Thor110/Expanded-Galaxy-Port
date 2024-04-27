16fb10d08ef01af5e50e5a469551915b92a66697
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-04-27 08:13:47 +0100

    EXE Resource Hack

--- Expanded Galaxy Port Installer/tslpatchdata/launcher.bat
+++ Expanded Galaxy Port Installer/tslpatchdata/launcher.bat
@@ -35,6 +35,7 @@ ren dialog.tlk dialog.tlk.main
 ren lips\001EBO_loc.mod 001EBO_loc.mod.main
 ren Modules\001ebo.mod 001ebo.mod.main
 ren StreamMusic\mus_sion.wav mus_sion.wav.main
+ren swkotor2.exe swkotor2.exe.main
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.main"
 
@@ -44,6 +45,7 @@ ren dialog.tlk.port dialog.tlk
 ren lips\001EBO_loc.mod.port 001EBO_loc.mod
 ren Modules\001ebo.mod.port 001ebo.mod
 ren StreamMusic\mus_sion.wav.port mus_sion.wav
+ren swkotor2.exe.port swkotor2.exe
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.port" "%%i"
 
@@ -59,6 +61,7 @@ ren dialog.tlk dialog.tlk.port
 ren lips\001EBO_loc.mod 001EBO_loc.mod.port
 ren Modules\001ebo.mod 001ebo.mod.port
 ren StreamMusic\mus_sion.wav mus_sion.wav.port
+ren swkotor2.exe swkotor2.exe.port
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.port"
 
@@ -68,6 +71,7 @@ ren dialog.tlk.main dialog.tlk
 ren lips\001EBO_loc.mod.main 001EBO_loc.mod
 ren Modules\001ebo.mod.main 001ebo.mod
 ren StreamMusic\mus_sion.wav.main mus_sion.wav
+ren swkotor2.exe.main swkotor2.exe
 
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.main" "%%i"
 
