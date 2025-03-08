dbcd2d0f9f34c1f108470e811cf96a6615bb04be
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-03-08 12:34:23 +0000

    LegacyPC Duplicate Message Removed

--- Launcher/FirstForm.cs
+++ Launcher/FirstForm.cs
@@ -174,11 +174,7 @@ namespace Launcher
             File.Move("lips\\001EBO_loc.mod.port", "lips\\001EBO_loc.mod");
             File.Move("Modules\\001ebo.mod.port", "Modules\\001ebo.mod");
             File.Move("StreamMusic\\mus_sion.wav.port", "StreamMusic\\mus_sion.wav");
-            if (File.Exists("DirectX/DSETUP.dll"))
-            {
-                MessageBox.Show("Class Changes Not Supported For The LegacyPC Version of the game on Steam currently.");
-            }
-            else
+            if (!File.Exists("DirectX/DSETUP.dll"))
             {
                 File.Move("swkotor2.exe.port", "swkotor2.exe");
             }
@@ -219,11 +215,7 @@ namespace Launcher
             File.Move("lips\\001EBO_loc.mod.main", "lips\\001EBO_loc.mod");
             File.Move("Modules\\001ebo.mod.main", "Modules\\001ebo.mod");
             File.Move("StreamMusic\\mus_sion.wav.main", "StreamMusic\\mus_sion.wav");
-            if (File.Exists("DirectX/DSETUP.dll"))
-            {
-                MessageBox.Show("Class Changes Not Supported For The LegacyPC Version of the game on Steam currently.");
-            }
-            else
+            if (!File.Exists("DirectX/DSETUP.dll"))
             {
                 File.Move("swkotor2.exe.main", "swkotor2.exe");
             }
