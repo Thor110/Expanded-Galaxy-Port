8cf4b4326f39fb1f9fa1b5cd9efc74fdfc16f1b3
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-03-19 21:38:16 +0000

    MAUI Mobile App Setup

---
+++ MobileLauncher/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileLauncher
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MobileLauncher/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileLauncher
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
